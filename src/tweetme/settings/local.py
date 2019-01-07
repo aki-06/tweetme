d9450e420c17ec95c22be894ba0b89799c9561f4
akihironemoto <akihiro.nemoto.06@gmail.com>
2019-01-07 23:10:34 +0900

    Serving Static Files

--- src/tweetme/settings/local.py
+++ src/tweetme/settings/local.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# manage.py
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
@@ -119,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static-storage"),
+]
+
+STATIC_ROOT =  os.path.join(os.path.dirname(BASE_DIR), "static-serve") 
