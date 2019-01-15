ece9a4d7bbfca0bec0154cbec75f7976a413dbba
akihironemoto <akihiro.nemoto.06@gmail.com>
2019-01-15 18:16:25 +0900

    Model Form

--- src/tweets/admin.py
+++ src/tweets/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
+from .forms import TweetModelForm
 from .models import Tweet
 
-admin.site.register(Tweet)
+
+class TweetModelAdmin(admin.ModelAdmin):
+    form = TweetModelForm
+
+
+admin.site.register(Tweet, TweetModelAdmin)
