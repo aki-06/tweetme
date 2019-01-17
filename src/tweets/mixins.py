f98aaa0a8698030ad02af3335c0704d86bee08a6
akihironemoto <akihiro.nemoto.06@gmail.com>
2019-01-17 22:36:56 +0900

    Update View

--- src/tweets/mixins.py
+++ src/tweets/mixins.py
@@ -2,11 +2,21 @@ from django import forms
 from django.forms.utils import ErrorList
 
 class FormUserNeededMixin(object):
-
+    """ログインしないとtweetできないようにするMixin"""
     def form_valid(self, form):
         if self.request.user.is_authenticated():
             form.instance.user = self.request.user
             return super(FormUserNeededMixin, self).form_valid(form)
         else:
             form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(['User must be logged in to continue'])
+            return self.form_invalid(form)
+
+
+class FormOwnerMixin(object):
+    """他ユーザーのtweetをupdateできないようにするMixin"""
+    def form_valid(self, form):
+        if form.instance.user == self.request.user:
+            return super(FormOwnerMixin, self).form_valid(form)
+        else:
+            form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(['This user is not allowed to change this data'])
             return self.form_invalid(form)
