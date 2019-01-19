350ed8745fd41936afa0461a9d5f0278872a7b14
akihironemoto <akihiro.nemoto.06@gmail.com>
2019-01-19 10:37:56 +0900

    Reverse URLs

--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -4,13 +4,13 @@ from .views import (
     TweetDeleteView,
     TweetDetailView,
     TweetListView,
-    TweetUpdateView
+    TweetUpdateView,
 )
 
 urlpatterns = [
     url(r'^$', TweetListView.as_view(), name='list'), # /tweet/
     url(r'^create/$', TweetCreateView.as_view(), name='create'), # /tweet/create/
-    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detal'), # /tweet/1/
+    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), # /tweet/1/
     url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'), # /tweet/1/update/
     url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'), # /tweet/1/delete/
 ]
