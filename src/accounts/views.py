fb772280c045d0940f0302f0098233e9fe0d9b0d
akihironemoto <akihiro.nemoto.06@gmail.com>
2019-01-30 20:25:11 +0900

    Model Manager for Follow Toggle

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
 from django.views.generic import DetailView
-
+from .models import UserProfile
 
 User = get_user_model()
 
@@ -11,3 +13,15 @@ class UserDetailView(DetailView):
     
     def get_object(self):
         return get_object_or_404(User, username__iexact=self.kwargs.get('username'))
+
+
+class UserFollowView(View):
+    def get(self, request, username, *args, **kwargs):
+        toggle_user = get_object_or_404(User, username__iexact=username)
+        if request.user.is_authenticated:
+            user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+            if toggle_user in user_profile.following.all():
+                user_profile.following.remove(toggle_user)
+            else:
+                user_profile.following.add(toggle_user)
+        return redirect('profiles:detail', username=username)
