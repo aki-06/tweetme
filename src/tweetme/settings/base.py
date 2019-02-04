8d96209640e3c88fc312ed29f2fe5305a2cc7e8f
akihironemoto <akihiro.nemoto.06@gmail.com>
2019-02-04 22:32:21 +0900

    HashTags App

--- src/tweetme/settings/base.py
+++ src/tweetme/settings/base.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'tweets.apps.TweetsConfig',
     'accounts.apps.AccountsConfig',
+    'hashtags.apps.HashtagsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
